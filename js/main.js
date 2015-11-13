2c6268a9bbd90db64d0bec7dad0384d9528198f2
Graphic <graphic@SOD-SJH-516-23M.local>
2015-11-13 13:28:18 -0500

    simon example

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
-// $('.blue')
-// 	.addClass('light')
-// 	.css('background-color', 'orange')
-// 	.text('This is a test!');
+var pattern = ['R', 'Y', 'B', 'R', 'G', 'Y', 'B'];
+var numb = 0;
 
+$('.panel').on('click', function(e) {
 
+	// Does the panel clicked match the current item
+	// in the sequence?
 
+	var colour = $(this).attr('data-colour');
 
-// OVER/OUT
-// When I hover over a panel, light it up
-$('.panel')
-	.on('mouseover', function() { // OVER
-		
-		console.log("OVER");
-		
-		// Add the light class
-		$(this).addClass('light');
-	})
-	.on('mouseout', function() { // OUT
-		
-		console.log("OUT");
-		
-		// Remove the light class
-		$(this).removeClass('light');
-	
-	});
+	if ( colour == pattern[numb] ) {
+		console.log("CORRECT :)")
+		// Short form of: numb = numb + 1;
+		numb++;
 
+			if (numb >= pattern.length ) {
+				console.log("YOU WON!");
+			}
+	}
+	else {
+		console.log("WRONG :(")
+	}
+
+});
 
 
 
